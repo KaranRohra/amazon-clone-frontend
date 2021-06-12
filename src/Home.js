bbb2fc7f62d25500b9aa16cce8763ee3606a269b
anishadhameja <68025041+anishadhameja@users.noreply.github.com>
2021-06-12 19:20:36 +0530

    [REF #3]: Changes in buttons effects

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
-import Product from './Product';
+import Product from "./Product";
+import Carousel from "react-bootstrap/Carousel";
 
 function Home() {
   return (
